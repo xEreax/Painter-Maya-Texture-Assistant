941f01d1569b9a333aa1156c148774c7478b3214
Edwina Asumang <22713605+xEreax@users.noreply.github.com>
2024-06-04 15:25:08 +0100

    [EA] create 2dTexture node and connect attributes to base color file

--- src/functions.py
+++ src/functions.py
@@ -64,6 +64,7 @@ def display_texture_files():
 
 def create_arnold_material_shader(selected_object_name: str):
     """Create AI Arnold Surface material shader"""
+    
     # Create aiStandard material
     material_name="aiStandardSurface"
     shader = cmds.shadingNode(material_name,
@@ -81,21 +82,56 @@ def create_arnold_material_shader(selected_object_name: str):
 
     return sg
 
+file_to_2dtexture_baseColor_attribute_list = [
+    "coverage",
+    "translateFrame"
+    "rotateFrame",
+    "mirrorU",
+    "mirrorV",
+    "stagger",
+    "wrapU",
+    "wrapV",
+    "repeatUV",
+    "offset",
+    "rotateUV",
+    "noiseUV",
+    "vertexUvOne",
+    "vertexUvTwo",
+    "vertexUvThree",
+    "vertexCameraOne",
+    ("outUV", "uv"),
+    ("outUvFilterSize", "uvFilterSize"),
+]
 
-def add_files_to_arnold_material(material_shader, file_path):
-    """Create file and add to arnorld material"""
-    # Note: Dealing with only baseColor currently
+def create_base_color_file(file_path, base_color_attribute_list):
+    "create file and connect attributes to 2d texture node"
 
     # Create file
     file_node = cmds.shadingNode("file", asTexture=True, isColorManaged=True)
     cmds.setAttr(f"{file_node}.fileTextureName", file_path, type="string")
 
-    #TODO: Connect file to a 2d texture node
+    # Create 2dTexture
+    texture_2d_node = cmds.shadingNode("place2dTexture", asUtility=True)
 
-    # Connect base color texture file to aiStandardSurface base color
+    # Connect file and 2dTexture attributes
+    for attribute in base_color_attribute_list:
+        if isinstance(attribute, str):
+            cmds.connectAttr(f"{texture_2d_node}.{attribute}",
+                             f"{file_node}.{attribute}",
+                             force=True)
+        elif isinstance(attribute, tuple):
+            cmds.connectAttr(f"{texture_2d_node}.{attribute[0]}",
+                             f"{file_node}.{attribute[1]}",
+                             force=True)
+
+    return file_node
+
+
+def add_base_color_file_to_arnold_material(material_shader, file_node):
+    """Add file to arnold material"""
     cmds.connectAttr(f"{file_node}.outColor", f"{material_shader}.baseColor", force=True)
 
 
-def assign_shader_to_object(sg, selected_object_name):
-    """Assign shader group to object"""
-    cmds.sets(selected_object_name, edit=True, forceElement=sg)
+def assign_material_to_object(material_sg, selected_object_name):
+    """Assign material shader group to object"""
+    cmds.sets(selected_object_name, edit=True, forceElement=material_sg)
