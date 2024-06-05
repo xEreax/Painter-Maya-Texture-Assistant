e022a0e039aa29559057ac998ecbc5425af86cc7
Edwina Asumang <22713605+xEreax@users.noreply.github.com>
2024-06-05 19:20:43 +0100

    [EA] clean up maya functions file

--- src/functions.py
+++ src/functions.py
@@ -3,35 +3,8 @@
 # author = Edwina Asumang
 #******************************************************************
 
-# Functions to create
-# get object, listen to maya selection
-# get/store name of object (or pointer to object)
-# Retrieve files
-# save list/dict of files {base_color: <file_path>, ...}
-# get name of object and file paths
-# Create arnold material THEN assign to object THEN add files
-# OR
-# Create arnold material THEN add files THEN assign to object
-
 import maya.cmds as cmds
 
-
-def get_current_selection_name():
-    """Gets first object in current selection"""
-    #TODO: Deal with case where object list is empty (no object selected)
-    selected_object_name: str
-    selected_objects: list = cmds.ls(selection=True, objectsOnly=True)
-    if selected_objects:
-        selected_object_name = selected_objects[0]
-    return selected_object_name
-
-
-def display_current_selection_name(selection_name: str):
-    """Display selected object name"""
-    #TODO: Set ui to selection name
-    return True
-
-#exr file type
 texture_file_dict = {
     "BaseColor": "",
     "Emissive": "",
@@ -40,47 +13,6 @@ texture_file_dict = {
     "Normal": "",
     "Roughness": "",
 }
-
-
-def get_project_directory():
-    """Get directory of project"""
-    #TODO: Get directory to project location
-    return True
-
-
-def populate_texture_file_dict(filenames: list):
-    """Put texture file names in dictionary"""
-    for filename in filenames:
-        for texture_file_type in texture_file_dict:
-            if texture_file_type in filename:
-                texture_file_dict[texture_file_type] = filename
-
-
-def display_texture_files():
-    """Display texture files"""
-    #TODO: Display file names in ui
-    return True
-
-
-def create_arnold_material_shader(selected_object_name: str):
-    """Create AI Arnold Surface material shader"""
-    
-    # Create aiStandard material
-    material_name="aiStandardSurface"
-    shader = cmds.shadingNode(material_name,
-                              asShader=True,
-                              name=f"{selected_object_name}_{material_name}")
-
-    # Create surface shader group
-    sg = cmds.sets(renderable=True,
-                   noSurfaceShader=True,
-                   empty=True,
-                   name=f"{selected_object_name}_{material_name}SG")
-
-    # Connect material to shader
-    cmds.connectAttr(f"{shader}.outColor", f"{sg}.surfaceShader", force=True)
-
-    return sg
 
 file_to_2dtexture_baseColor_attribute_list = [
     "coverage",
@@ -102,6 +34,45 @@ file_to_2dtexture_baseColor_attribute_list = [
     ("outUV", "uv"),
     ("outUvFilterSize", "uvFilterSize"),
 ]
+
+def get_current_selection_name():
+    """Gets first object in current selection"""
+    #TODO: Deal with case where object list is empty (no object selected)
+    selected_object_name: str
+    selected_objects: list = cmds.ls(selection=True, objectsOnly=True)
+    if selected_objects:
+        selected_object_name = selected_objects[0]
+    return selected_object_name
+
+
+def populate_texture_file_dict(filenames: list):
+    """Put texture file names in dictionary"""
+    for filename in filenames:
+        for texture_file_type in texture_file_dict:
+            if texture_file_type in filename:
+                texture_file_dict[texture_file_type] = filename
+
+
+def create_arnold_material_shader(selected_object_name: str):
+    """Create AI Arnold Surface material shader"""
+
+    # Create aiStandard material
+    material_name="aiStandardSurface"
+    shader = cmds.shadingNode(material_name,
+                              asShader=True,
+                              name=f"{selected_object_name}_{material_name}")
+
+    # Create surface shader group
+    sg = cmds.sets(renderable=True,
+                   noSurfaceShader=True,
+                   empty=True,
+                   name=f"{selected_object_name}_{material_name}SG")
+
+    # Connect material to shader
+    cmds.connectAttr(f"{shader}.outColor", f"{sg}.surfaceShader", force=True)
+
+    return sg
+
 
 def create_base_color_file(file_path, base_color_attribute_list):
     "create file and connect attributes to 2d texture node"
